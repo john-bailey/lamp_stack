719e775238a5269b515702c6ad2ef8bf4e5c3787
John Bailey <john.e.bailey@gamil.com>
2021-03-18 19:46:04 -0500

    added gitignore and fixed source

--- local/initdb.sql
+++
@@ -1,7 +0,0 @@
-RENAME USER 'root'@'%' TO 'admin'@'10.0.26.%';
-RENAME USER 'appdev'@'%' TO 'appdev'@'10.0.26.%';
-
-GRANT ALL PRIVILEGES ON testdb.* TO 'appdev'@'10.0.26.%';
-GRANT SELECT ON mysql.db TO 'appdev'@'10.0.26.%';
-
-FLUSH PRIVILEGES;
